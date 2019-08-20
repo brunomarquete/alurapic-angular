bc4cf2c17b9890d792fb49c38ec7dee7990d88a4
brunomarquete <brunomarquete@gmail.com>
2019-08-20 15:16:12 -0300

    Aula 1 do curso 2 de Angular

--- alurapic/src/app/home/home.module.ts
+++ alurapic/src/app/home/home.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { SigninComponent } from './signin/signin.component';
 import { ReactiveFormsModule} from '@angular/forms'
 import { MessageModule } from '../shared/components/message/message.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [SigninComponent],
   imports: [
     CommonModule,
     ReactiveFormsModule,
-    MessageModule
+    MessageModule,
+    RouterModule
   ]
 })
 export class HomeModule { }
--- alurapic/src/app/home/signin/signin.component.ts
+++ alurapic/src/app/home/signin/signin.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/core/auth.service';
+import { Router } from '@angular/router';
+import { PlatformDetectorService } from 'src/app/core/platform-detector.service';
 
 @Component({
   selector: 'app-signin',
@@ -10,7 +13,30 @@ export class SigninComponent implements OnInit {
 
   formGroup : FormGroup;
 
-  constructor(private formBuilder : FormBuilder) { }
+  @ViewChild('inputUserName') inputUserName : ElementRef<HTMLInputElement>
+
+  constructor(private formBuilder : FormBuilder,
+              private authService: AuthService,
+              private router: Router,
+              private platformDetectorService : PlatformDetectorService) { }
+
+  login() {
+
+    const userName = this.formGroup.get('userName').value;
+    const password = this.formGroup.get('password').value;
+
+    this.authService.authenticate(userName, password).subscribe(
+       () => this.router.navigate(['user', userName]),
+       err => {
+         console.log(err);
+         if (this.platformDetectorService.isPlatformBrowser()) {
+          this.inputUserName.nativeElement.focus();
+         }
+         this.formGroup.reset();
+       }
+    );
+
+  }
 
   ngOnInit() {
     this.formGroup = this.formBuilder.group({
